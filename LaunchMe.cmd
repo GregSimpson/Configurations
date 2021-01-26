6adad49b88b5675106faf33bf727f5b8cdb78049
Greg Simpson <gsimpson@pobox.com>
2021-01-26 09:05:03 -0700

    new

---
+++ LaunchMe.cmd
@@ -0,0 +1,26 @@
+
+run "C:\Users\eloy98104\OneDrive - TeleTech Holdings, Inc\newLaptopStorage\gjs\Configurations\cygwin\Noise.exe"
+
+run "C:\Program Files (x86)\Cisco\Cisco AnyConnect Secure Mobility Client\vpnui.exe"
+sleep 180
+
+
+REM https://www.ghacks.net/2013/10/06/list-useful-google-chrome-command-line-switches/ 
+
+run "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -new-window https://connect.xfinity.com/appsuite/#!!&app=io.ox/mail&folder=default0/INBOX 
+run "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -d https://www.coursera.org/professional-certificates/gcp-data-engineering
+run "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -d https://www.wunderground.com/weather/us/co/parker/39.55,-104.77
+
+run "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -new-window https://ttec.cloud.looker.com/folders/home
+
+run "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -new-window https://humanify.atlassian.net/secure/RapidBoard.jspa?rapidView=44&projectKey=DWF&view=planning&issueLimit=100
+run "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -d https://github.com/humanifydev/project-fischer/wiki/About-Onboarding
+run "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -d https://teletechinc.sharepoint.com/sites/mosaicIntranetProdHome
+run "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -d https://humanify.atlassian.net/wiki/home
+
+
+run "C:\Users\eloy98104\AppData\Local\slack\slack.exe"
+run "C:\Program Files (x86)\Microsoft Office\root\Office16\OUTLOOK.EXE"
+run C:\Users\eloy98104\AppData\Local\Microsoft\Teams\Update.exe --processStart "Teams.exe"
+
+
