8737c6278342a1f3d08b2c2ca811a62a97f54d99
GregSimpson <GregSimpson@users.noreply.github.com>
2021-05-07 15:26:24 -0600

    Update rp_run_client.sh

--- ubuntu/rp_run_client.sh
+++ ubuntu/rp_run_client.sh
@@ -4,15 +4,15 @@
 
 # # RealPlay Server
 # export AWS_DEFAULT_REGION=us-west-2
-# export REALPLAY_DB_URI=postgres://realplayuser:F35D6492-CC53-4F38-BA0C-DA8500D793C9@ovpc1-pgsql01.clrygru5fh3l.us-west-2.rds.amazonaws.com:5432/realplay_dce1
+# export REALPLAY_DB_URI=p
 # # CE05
-# #export REALPLAY_DB_URI=postgres://realplayuser:4A51E8B3-71B7-41A5-8519-6AD4233258A1@app1-pgsql01.c7e4lfnihqtl.us-east-1.rds.amazonaws.com:5432/realplay_ce05
+# #export REALPLAY_DB_URI=p
 # 
 
 # export REALPLAY_LOG_PATH='./gunicorn.log'
 # export GOOGLE_APPLICATION_CREDENTIALS='./keys/RealPlay-Authorization.json'
 # #Dev - dce1
-# export RULE_ENGINE_URL="http://10.12.13.171:30378"
+# export RULE_ENGINE_URL="http://xxx"
 # export REDIS_HOST=localhost
 # export REDIS_PORT=7000
 # export REDIS_CACHE_EXPIRY_SECS=3600
@@ -40,7 +40,7 @@
 # REACT_APP_API_URL=http://localhost:5000
 # # RealPlay Server
 # export AWS_DEFAULT_REGION=us-west-2
-# export REALPLAY_DB_URI=postgres://realplayuse.com:5432/realplay_dce1
+# export REALPLAY_DB_URI=postgres://XXXYYYZZZ.com:5432/XXXYYY_dce1
 # # CE05
 # export REALPLAY_LOG_PATH='./gunicorn.log'
 # export GOOGLE_APPLICATION_CREDENTIALS='./keys/RealPlay-Authorization.json'
@@ -50,7 +50,7 @@
 # export REDIS_PORT=7000
 # export REDIS_CACHE_EXPIRY_SECS=3600
 # 
-# #Request URL: http://localhost:5000/scenarios/tenant/parasol/state/prod?group_by=lob
+# #Request URL: http://localh
 # # RealPlay
 # # RealPlay Client
 # export CUSTOM_BUILD="parasol"
