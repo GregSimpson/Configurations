f437716eb1bd5d3230bcd4a28a85fd2e76ddf7f9
GregSimpson <GregSimpson@users.noreply.github.com>
2021-05-07 15:25:04 -0600

    Update rp_run_client.sh

--- ubuntu/rp_run_client.sh
+++ ubuntu/rp_run_client.sh
@@ -23,11 +23,11 @@
 
 # RealPlay Client
 ### export CUSTOM_BUILD="parasol"
-### export POSTGRES_PASS="password"
+### export POSTGRES_PASS="XXXXXXX"
 # export REACT_APP_LOCAL_TENANT="parasol"
 #export REACT_APP_API_URL="http://localhost:5000"
 # export REACT_APP_API_URL=http://localhost:5000
-#export REACT_APP_API_URL="https://reaplayserver.dce1.humanify.com"
+#export REACT_APP_API_URL="https://(dce1)"
 # export RUNTIME_ENV=dce1
 
 #############################################################
@@ -40,13 +40,12 @@
 # REACT_APP_API_URL=http://localhost:5000
 # # RealPlay Server
 # export AWS_DEFAULT_REGION=us-west-2
-# export REALPLAY_DB_URI=postgres://realplayuser:F35D6492-CC53-4F38-BA0C-DA8500D793C9@ovpc1-pgsql01.clrygru5fh3l.us-west-2.rds.amazonaws.com:5432/realplay_dce1
+# export REALPLAY_DB_URI=postgres://realplayuse.com:5432/realplay_dce1
 # # CE05
-# #export REALPLAY_DB_URI=postgres://realplayuser:4A51E8B3-71B7-41A5-8519-6AD4233258A1@app1-pgsql01.c7e4lfnihqtl.us-east-1.rds.amazonaws.com:5432/realplay_ce05
 # export REALPLAY_LOG_PATH='./gunicorn.log'
 # export GOOGLE_APPLICATION_CREDENTIALS='./keys/RealPlay-Authorization.json'
 # #Dev - dce1
-# export RULE_ENGINE_URL="http://10.12.13.171:30378"
+
 # export REDIS_HOST=localhost
 # export REDIS_PORT=7000
 # export REDIS_CACHE_EXPIRY_SECS=3600
@@ -57,7 +56,7 @@
 # export CUSTOM_BUILD="parasol"
 # export REACT_APP_LOCAL_TENANT="parasol"
 # export REACT_APP_API_URL="http://localhost:5000"
-# #export REACT_APP_API_URL="https://reaplayserver.dce1.humanify.com"
+
 # export RUNTIME_ENV=dce1
 
 #-----------------
